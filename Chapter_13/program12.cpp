a9479c49bbfbe74a556067eb5200690deb915b02
Connor Morgan <80083299+HiConnorM@users.noreply.github.com>
2022-04-27 17:20:45 -0500

    Adding files for payroll project

---
+++ Chapter_13/program12.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+
+class payroll
+{
+private:
+    int employees_cm;
+    double hrworked_cm, payrate_cm, total_pay_cm;
+
+    void getinfo()
+    {
+
+    }
+
+    void calcPay()
+    {
+
+    }
+
+    void putinfo()
+    {
+
+    }
+
+public:
+    
+    
+
+};
+
+
+int main()
+{
+
+
+
+    return 0;
+}
